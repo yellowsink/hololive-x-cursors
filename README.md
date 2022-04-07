be71ecbd7dafede143c4d7373cfcafdabbe6c9ae
Cain Atkinson <yellowsink@riseup.net>
2022-04-07 18:24:40 +0100

    readme & refine script

---
+++ README.md
@@ -0,0 +1,8 @@
+# Takanashi X Cursors
+
+A set of cursors for X featuring Takanashi Kiara.
+
+[Drawn by @growowling](https://twitter.com/growowling/status/1510479966491725829)
+and converted to X cursors [with permission](https://twitter.com/growowling/status/1510743060346728451).
+
+![Preview gif](https://raw.githubusercontent.com/yellowsink/takanashi-x-cursors/master/preview.gif)
--- index.mjs
+++ index.mjs
@@ -1,7 +1,9 @@
 #!/usr/bin/env zx
+import { fs } from "zx";
+import { cd } from "zx";
 import "zx/globals";
 
-const { readdir, emptyDir, writeFile } = fs;
+const { readdir, emptyDir, writeFile, remove, move } = fs;
 
 /*\
 |*| data
@@ -11,25 +13,30 @@ const { readdir, emptyDir, writeFile } = fs;
 const curSize = 32;
 const animDelay = 100;
 const cursors = {
-  //default: ["kiara", 5, 5],
-  default: ["kiara mouse", 0, 0],
-  //default: ["kiara sword", 0, 0],
+  //left_ptr: ["kiara", 5, 5],
+  left_ptr: ["kiara mouse", 0, 0],
+  //left_ptr: ["kiara sword", 0, 0],
   size_fdiag: ["kiarabird", 15, 15],
   size_bdiag: ["kiarabird2", 15, 15],
-  size_ver: ["kiarabird3", 15, 15],
-  size_hor: ["kiarabird4", 15, 15],
+  sb_v_double_arrow: ["kiarabird3", 15, 15],
+  sb_h_double_arrow: ["kiarabird4", 15, 15],
   fleur: ["kiarabird5", 15, 15],
-  pointer: ["kiara finger", 10, 3],
+  hand2: ["kiara finger", 10, 3],
   //progress: ["kiara finger2", 10, 3],
   progress: ["kiara finger3", 10, 3],
   //progress: ["kiarasleep2", 0, 0],
-  wait: ["kiarasleep", 16, 18],
-  //wait: ["kiarasleep2", 0, 0],
-  text: ["kiara text", 3, 8],
+  watch: ["kiarasleep", 16, 18],
+  //watch: ["kiarasleep2", 0, 0],
   xterm: ["kiara text", 3, 8],
   "not-allowed": ["kiara x", 15, 24],
   "no-drop": ["kiara x", 15, 24],
 };
+// feel free to modify the Inherits line
+const themeIndex = `[Icon Theme]
+Name=Takanashi Kiara (art: @growowling)
+Comment=A set of cursors for X featuring Takanashi Kiara
+Inherits=breeze_cursors`;
+const installPath = `${process.env.HOME}/.icons/takanashi-cursors`;
 
 // convert all gifs to pngs
 await emptyDir("png");
@@ -44,13 +51,25 @@ for (const destName in cursors) {
   const [, hotX, hotY] = cursors[destName];
   const matchedFiles = pngList.filter((f) => f.startsWith(destName));
 
-  const cfg = matchedFiles.map(f => `${curSize} ${hotX} ${hotY} ${f} ${animDelay}`).join("\n");
-  await writeFile(`png/${destName}.cursor`, cfg)
+  const cfg = matchedFiles
+    .map((f) => `${curSize} ${hotX} ${hotY} ${f} ${animDelay}`)
+    .join("\n");
+  await writeFile(`png/${destName}.cursor`, cfg);
 }
 
 // generate cursors
-await emptyDir("out");
+await emptyDir("out/cursors");
 await cd("png");
 for (const destName in cursors) {
-  await $`xcursorgen ${destName}.cursor ../out/${destName}`;
+  await $`xcursorgen ${destName}.cursor ../out/cursors/${destName}`;
+}
+await cd("..");
+
+// generate actual theme
+await writeFile("out/index.theme", themeIndex);
+
+const install = await question("Install theme to ~/.icons? [y/N]");
+if (install === "y") {
+  await remove(installPath);
+  await move("out", installPath);
 }
